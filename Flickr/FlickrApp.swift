b3d3529396eef08578f0840a6501d1684343c1e2
Max Vynnyk <shwepster@icloud.com>
2024-09-18 14:09:09 +0300

    Initial Commit

---
+++ Flickr/FlickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrApp.swift
+//  Flickr
+//
+//  Created by Maxim Vynnyk on 18.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FlickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
