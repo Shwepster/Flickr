48100e207c7003432e5d436c5d5cc821c711d0a7
Max Vynnyk <shwepster@icloud.com>
2024-12-13 18:31:34 +0200

    update title

--- FlickrWatch Watch App/History/HistoryView.swift
+++ FlickrWatch Watch App/History/HistoryView.swift
@@ -11,35 +11,28 @@ struct HistoryView: View {
     @State private var viewModel = ViewModel()
     
     var body: some View {
-        ScrollView {
-            title
-            
-            if viewModel.items.isEmpty {
-                emptyView
-            } else {
-                ForEach(viewModel.items) { item in
-                    Text(item.text)
-                        .frame(height: 40)
-                        .frame(maxWidth: .infinity)
-                        .background(.fill)
-                        .clipShape(.capsule)
+        NavigationView {
+            ScrollView {
+                if viewModel.items.isEmpty {
+                    emptyView
+                } else {
+                    ForEach(viewModel.items) { item in
+                        Text(item.text)
+                            .frame(height: 40)
+                            .frame(maxWidth: .infinity)
+                            .background(.fill)
+                            .clipShape(.capsule)
+                    }
+                    .transition(.move(edge: .bottom))
                 }
-                .transition(.move(edge: .bottom))
             }
+            .background(Color.black)
+            .ignoresSafeArea(.all, edges: .bottom)
+            .task {
+                viewModel.onCreated()
+            }
+            .navigationTitle("Flickr History")
         }
-        .background(Color.black)
-        .ignoresSafeArea(.all, edges: .bottom)
-        .task {
-            viewModel.onCreated()
-        }
-    }
-    
-    @ViewBuilder
-    private var title: some View {
-        Text("Flickr Search History")
-            .font(.headline)
-            .padding(.bottom)
-            .foregroundStyle(.primary.opacity(0.9))
     }
     
     @ViewBuilder
